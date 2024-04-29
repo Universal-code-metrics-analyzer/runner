f4b219c0ff93bc5cd57dfddf4220c11335964bfe
Alex Pirogov <alex-pirogov@qa.studio>
2024-04-29 16:44:45 +0300

    feat: git processor plugin

--- app/main.py
+++ app/main.py
@@ -1,10 +1,16 @@
 import asyncio
 
 from app.config import config
+from app.plugins import plugin_manager
 
 
 async def main():
-    print(config)
+    git_processor_class = plugin_manager.get_git_processor_class(config.git_processor.plugin)
+    git_processor = git_processor_class(
+        config.git_processor.config, commit_sha='1c81a619be55cb2855507eeb2e633e63997ce3e5'
+    )
+
+    print(await git_processor.process())
 
 
 if __name__ == '__main__':
